e44382b3e8e4f13d00e9df8ed9382679b92f7f0e
wenliangtu <twl-twl@sjtu.edu.cn>
2023-12-15 11:33:55 +0800

    delete debug section

--- vmm.cpp
+++ vmm.cpp
@@ -42,28 +42,9 @@ int main() {
 
     for(int i = 0; i < n * n / 4; )
     {
-        // for(int j = 0; j < 4; j++)
-        // {
-        //     std::cout << "*(m + j) is " << *(m) << std::endl; 
-        //     std::cout << "*(v + j) is " << *(v) << std::endl; 
-        //     m++;
-        //     if(i * 4 % n == 0)
-        //     {
-        //         v = ttt;
-        //     }
-        //     else
-        //     {
-        //         v++;
-        //     }
-        // }
-        
         *Tmp1 = _mm256_add_epi64(*Tmp1, _mm256_mullo_epi32(*m1, *v1));
-        
         m1++;
         i++;
-        // std::cout << "i is " << i << std::endl;
-        // std::cout << "i * 4 mod n is " << i * 4 % n << std::endl;
-        // std::cout << "i * 4 / n is " << i * 4 / n << std::endl;
         if(i * 4 % n == 0)
         {
             v1 = temp;
@@ -71,24 +52,13 @@ int main() {
             { 
                 res[i * 4 / n - 1] += Tmp[k];
                 Tmp[k] = 0;
-                //std::cout << "Now k is " << k << " and Tmp[k] is " << Tmp[k] << std::endl;
             }
         }
         else
         {
             v1++;
         }
-
     }
-    //std::cout << "n is " << n << std::endl;
-
-    // long long * tmp = res;
-    // for(int i = 0; i < n; i++)
-    // {
-    //     //std::cout << "res is " << *(tmp) << std::endl;
-    //     tmp++;
-    // }
-
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
@@ -96,9 +66,9 @@ int main() {
         ouf << res[i] << " ";
     }
 
-    // free(v);
-    // free(m);
-    // free(res);
+    free(v);
+    free(m);
+    free(res);
 
     std::cout << "After AVX acceleration, execution time taken: " << duration.count() << " milliseconds" << std::endl;
     return 0;
